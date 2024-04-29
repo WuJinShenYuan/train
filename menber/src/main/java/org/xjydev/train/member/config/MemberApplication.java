5a3f5d225eae49b33140542b7c722ca474d0a0d0
WuJinShenYuan <2876700265@qq.com>
2024-04-29 23:18:04 +0800

    4.3 新增member会员模块

---
+++ menber/src/main/java/org/xjydev/train/member/config/MemberApplication.java
@@ -0,0 +1,15 @@
+package org.xjydev.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("org.xjydev")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
